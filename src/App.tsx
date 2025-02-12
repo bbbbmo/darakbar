78cb720526ff4c9dffbf95525398e1007985fb9f
bbbbmo <bbbbmo@github.com>
2025-02-12 16:38:26 +0900

    프로젝트 기본 설정/styled-component, router

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+
+function App() {
+  const router = createBrowserRouter([
+    {
+      path: "/",
+      element: "",
+    },
+  ]);
+  return (
+    <>
+      <RouterProvider router={router} />
+    </>
+  );
+}
+
+export default App;
