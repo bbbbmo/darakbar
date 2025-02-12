4296e8ec15a902b7f7fad7e8448d0ecf527da6fd
bbbbmo <aqw20501@naver.com>
2025-02-12 22:50:05 +0900

    postcss.config.js 오류 수정

--- src/App.tsx
+++ src/App.tsx
@@ -1,36 +1,15 @@
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
-import styled, { createGlobalStyle } from "styled-components";
-import reset from "styled-reset";
+import Home from "./routes/Home";
 
 const router = createBrowserRouter([
   {
     path: "/",
-    element: "",
+    element: <Home />,
   },
 ]);
 
-const GlobalStyle = createGlobalStyle`
-${reset}
-* {
-  box-sizing: border-box;
-}
-body {
-font-family: system-ui, -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif
-}
-`;
-
-const Wrapper = styled.div`
-  width: 100%;
-  height: 100vh;
-`;
-
 function App() {
-  return (
-    <Wrapper>
-      <GlobalStyle />
-      <RouterProvider router={router} />
-    </Wrapper>
-  );
+  return <RouterProvider router={router} />;
 }
 
 export default App;
--- src/routes/Home.tsx
+++ src/routes/Home.tsx
@@ -1,13 +1,3 @@
-import styled from "styled-components";
-
-const Wrapper = styled.div`
-  width: 100px;
-  height: 100vh;
-`;
-
 export default function Home() {
-    
-  return <Wrapper>
-안녕
-  </Wrapper>;
+  return <div></div>;
 }
