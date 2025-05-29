3d4070244f7d515908a8658297dd4658aa2fcf01
bbbbmo <aqw20501@naver.com>
2025-05-29 22:23:57 +0900

    ➕ add: flowbite react 추가

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,9 @@
 import { defineConfig } from "vite";
 import tailwindcss from "@tailwindcss/vite"; // tailwindcss 설정
 import react from "@vitejs/plugin-react";
+import flowbiteReact from "flowbite-react/plugin/vite";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
+  plugins: [react(), tailwindcss(), flowbiteReact()],
 });
