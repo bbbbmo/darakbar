db695ffca2245fb4f5b45085bc912964eb582c0c
bbbbmo <aqw20501@naver.com>
2025-10-29 22:08:48 +0900

    🐛 fix(ReviewLike): 낙관적 업데이트 제대로 동작하지 않던 버그 수정/아이콘 하트로 변경

--- src/app/(main)/bar-search/[barId]/_components/Footer/Review/ReviewCard/ReviewLike.tsx
+++ src/app/(main)/bar-search/[barId]/_components/Footer/Review/ReviewCard/ReviewLike.tsx
@@ -4,7 +4,7 @@ import { deleteBarReviewLike } from '@/lib/supabase/api/review/likes/deleteBarRe
 import { postBarReviewLike } from '@/lib/supabase/api/review/likes/postBarReviewLike'
 import { useMutation, useQueryClient } from '@tanstack/react-query'
 import { useState } from 'react'
-import { HiOutlineThumbUp } from 'react-icons/hi'
+import { HiHeart } from 'react-icons/hi'
 import { produce } from 'immer'
 import clsx from 'clsx'
 import { UserData } from '@/stores/auth.store'
@@ -23,65 +23,82 @@ export default function ReviewLike({ review, userData }: ReviewLikeProps) {
 
   const [isLiked, setIsLiked] = useState<boolean>(isLikedByCurrentUser)
 
-  const { mutate: toggleLike } = useMutation({
+  const optimisticUpdate = (likeCount: 1 | -1) => async () => {
+    await queryClient.cancelQueries({
+      queryKey: ['bar-reviews', String(review.bar_id)],
+    })
+
+    const previousReviews = queryClient.getQueryData([
+      'bar-reviews',
+      String(review.bar_id),
+    ])
+
+    queryClient.setQueryData(
+      ['bar-reviews', String(review.bar_id)],
+      (old: { data: BarReview[] }) => {
+        return {
+          ...old,
+          data: produce(old.data, (draft) => {
+            const reviewIndex = draft.findIndex((r) => r.id === review.id)
+            if (reviewIndex !== -1) {
+              draft[reviewIndex].like_count += likeCount
+            }
+          }),
+        }
+      },
+    )
+
+    return { previousReviews }
+  }
+
+  const { mutate: likeMutate } = useMutation({
     mutationFn: async () => {
-      if (!userData?.id) {
-        snackBar.showError('실패', '로그인 후 좋아요를 누를 수 있습니다.')
-        return
-      }
-      if (isLiked) {
-        // 좋아요 취소
-        await deleteBarReviewLike(review.id, userData?.id)
-      } else {
-        // 좋아요 추가
-        await postBarReviewLike(review.id, userData?.id)
-      }
-      setIsLiked((prev) => !prev)
+      await postBarReviewLike(review.id, userData!.id)
     },
-    onMutate: async () => {
-      await queryClient.cancelQueries({
-        queryKey: ['bar-reviews', String(review.bar_id)],
-      })
-
-      const previousReviews = queryClient.getQueryData([
-        'bar-reviews',
-        String(review.bar_id),
-      ])
-
-      queryClient.setQueryData(
-        ['bar-reviews', String(review.bar_id)],
-        (old: { data: BarReview[] }) => {
-          return {
-            ...old,
-            data: produce(old.data, (draft) => {
-              const reviewIndex = draft.findIndex((r) => r.id === review.id)
-              if (reviewIndex !== -1) {
-                isLiked
-                  ? draft[reviewIndex].like_count--
-                  : draft[reviewIndex].like_count++
-              }
-            }),
-          }
-        },
-      )
-
-      return { previousReviews }
-    },
+    onMutate: optimisticUpdate(1),
     onError: (_err, _variables, context) => {
       queryClient.setQueryData(
         ['bar-reviews', String(review.bar_id)],
         context?.previousReviews,
       )
     },
+    onSuccess: () => {
+      setIsLiked(true)
+    },
   })
+
+  const { mutate: unlikeMutate } = useMutation({
+    mutationFn: async () => {
+      await deleteBarReviewLike(review.id, userData!.id)
+    },
+    onMutate: optimisticUpdate(-1),
+    onError: (_err, _variables, context) => {
+      queryClient.setQueryData(
+        ['bar-reviews', String(review.bar_id)],
+        context?.previousReviews,
+      )
+    },
+    onSuccess: () => {
+      setIsLiked(false)
+    },
+  })
+
+  const handleClick = () => {
+    if (!userData?.id) {
+      snackBar.showError('실패', '로그인 후 좋아요를 누를 수 있습니다.')
+      return
+    }
+    isLiked ? unlikeMutate() : likeMutate()
+  }
+
   return (
     <div
       className="flex cursor-pointer items-center gap-1 rounded-md p-2 transition-all duration-200 ease-in-out hover:bg-neutral-500 hover:text-neutral-800"
-      onClick={() => toggleLike()}
+      onClick={handleClick}
     >
-      <HiOutlineThumbUp
+      <HiHeart
         size={20}
-        className={clsx(isLiked ? 'fill-amber-400 text-amber-400' : 'none')}
+        className={clsx(isLiked ? 'fill-red-400 text-red-400' : 'none')}
       />
       {review.like_count || 0}개
     </div>
