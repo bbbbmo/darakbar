a97e370529b237b1ff9334cc9a056a98ed581293
bbbbmo <bbbbmo@github.com>
2025-12-04 18:00:28 +0900

    💄style: 아이콘 추가

--- src/app/(main)/bar-news/_components/PostTabs.tsx
+++ src/app/(main)/bar-news/_components/PostTabs.tsx
@@ -2,6 +2,12 @@
 
 import { TabItem, Tabs, TabsRef } from 'flowbite-react'
 import { useRef } from 'react'
+import {
+  HiOutlineCake,
+  HiOutlineSparkles,
+  HiOutlineSpeakerphone,
+  HiOutlineTrendingUp,
+} from 'react-icons/hi'
 
 export default function PostTabs() {
   const tabsRef = useRef<TabsRef>(null)
@@ -9,10 +15,10 @@ export default function PostTabs() {
   return (
     <div className="sticky top-18 z-10 opacity-90">
       <Tabs aria-label="Post tabs" ref={tabsRef} variant="fullWidth">
-        <TabItem active title="전체"></TabItem>
-        <TabItem title="신메뉴"></TabItem>
-        <TabItem title="이벤트"></TabItem>
-        <TabItem title="소식"></TabItem>
+        <TabItem icon={HiOutlineTrendingUp} active title="전체"></TabItem>
+        <TabItem icon={HiOutlineSparkles} title="신메뉴"></TabItem>
+        <TabItem icon={HiOutlineCake} title="이벤트"></TabItem>
+        <TabItem icon={HiOutlineSpeakerphone} title="소식"></TabItem>
       </Tabs>
     </div>
   )
